f291cde1bbe49f90518a6b5e605127d581e19305
Nosheen-pixel <nosheen.bibi1992@gmail.com>
2024-11-15 12:46:17 +0000

    activity 10 completed
    Everything is working fine

--- src/components/screens/ModuleListScreen.js
+++ src/components/screens/ModuleListScreen.js
@@ -1,21 +1,57 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { useState } from "react";
+import { LogBox, StyleSheet } from "react-native";
+import Screen from "../layout/Screen";
+//import ignoreWarnings from "../../utils/ignoreWarnings";
+import ModuleList from "../entity/modules/ModuleList";
+import initialModules from "../../data/modules";
+import { Button, ButtonTray } from "../UI/Button";
+import Icons from "../UI/Icons";
+import ModuleAddScreen from "./ModuleAddScreen";
+import ModuleViewScreen from "./ModuleViewScreen";
 
-const ModuleListScreen = () => {
+const ModuleListScreen = ({ navigation }) => {
+  //Initialisations.............
+  //ignoreWarnings();
+  LogBox.ignoreLogs([
+    "Non-serializable values were found in the navigation state",
+  ]);
+  //let modules = initialModules;
+  //State.....
+  const [modules, setModules] = useState(initialModules);
+
+  //Handlers....
+
+  const handleDelete = (module) =>
+    setModules(modules.filter((item) => item.ModuleID !== module.ModuleID));
+
+  const handleAdd = (module) => setModules([...modules, module]);
+
+  const onDelete = (module) => {
+    handleDelete(module);
+    navigation.goBack();
+  };
+
+  const onAdd = (module) => {
+    handleAdd(module);
+    navigation.goBack();
+  };
+
+  const gotoViewScreen = (module) =>
+    navigation.navigate("ModuleViewScreen", { module, onDelete });
+  const gotoAddScreen = () => navigation.navigate("ModuleAddScreen", { onAdd });
+
+  // {if (item.ModuleID !== module.ModuleID) return true;
+  //else return false; });
+
+  //view....
   return (
-    <View style={styles.container}>
-      <Text>List</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Screen>
+      <ButtonTray>
+        <Button label={"Add"} icon={<Icons.Add />} onClick={gotoAddScreen} />
+      </ButtonTray>
+      <ModuleList modules={modules} onSelect={gotoViewScreen} />
+    </Screen>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
+const styles = StyleSheet.create({});
 export default ModuleListScreen;
---
+++ src/components/screens/ModuleModifyScreen.js
@@ -0,0 +1,17 @@
+import { StyleSheet, Text } from "react-native";
+import Screen from "../layout/Screen";
+
+const ModuleModifyScreen = () => {
+  //Initialisations.............
+  //State.....
+  //Handlers....
+  //view....
+  return (
+    <Screen>
+      <Text>Modify</Text>
+    </Screen>
+  );
+};
+
+const style = StyleSheet.create({});
+export default ModuleModifyScreen;
